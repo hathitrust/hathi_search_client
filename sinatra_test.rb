3dfdef3fefc05ca479cd96763b0cef57c28a2044
Josh Steverman <jstever@umich.edu>
2021-04-16 17:03:28 -0400

    search form with added terms

--- sinatra_test.rb
+++ sinatra_test.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+$LOAD_PATH << './lib'
 require 'sinatra'
+
+require 'query'
+require 'search'
 
 def authenticate!
   halt 403 unless request.get_header('HTTP_X_REMOTE_USER') == 'admin@default.invalid'
@@ -10,6 +14,12 @@ before do
   #authenticate!
 end
 
-get '/' do
+get '/search-client' do
+  @indexes = params[:indexes] || []
+  @terms = (params[:terms] || []).reject {|t| t == ''}
+  if @indexes.any? and @terms.any?
+    @indexes << "author2" if @indexes.include? "author"
+    @s = Search.new(@indexes, @terms)
+  end
   erb :search_form 
 end
