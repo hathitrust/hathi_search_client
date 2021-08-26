54f38085756db6cbb2160e99e9fae506d4023e53
Josh Steverman <jstever@umich.edu>
2021-08-26 11:54:03 -0400

    Add subject (topicStr) search
    Extract record source code HOL$s from MARC

--- spec/catalog_record_spec.rb
+++ spec/catalog_record_spec.rb
@@ -15,8 +15,9 @@ RSpec.describe CatalogRecord do
       'publisher' => ['Australian National University ;', 'Distributed by ANU Press,'],
       'ht_json' => '[{"htid":"uva.x001592955","newly_open":null,"ingest":"20201208","rights":["cc-by-nc-nd-4.0",null],"heldby":["anu","asu","auburn","auckland","universityofcalifornia","brown","bu","columbia","cornell","dartmouth","duke","harvard","hawaii","iastate","illinois","iu","jhu","ksu","miami","monash","msu","northwestern","nypl","ox","princeton","smith","stanford","ua","uchicago","uiowa","umass","umich","umn","unc","unimelb","unsw","upenn","uq","usg","utexas","utoronto","uwo","virginia","wisc","yorku"],"collection_code":"uva","enum_pubdate":"1983","enum_pubdate_range":"1980-1989","dig_source":"google"},{"htid":"inu.30000000079024","newly_open":null,"ingest":"20200525","rights":["cc-by-nc-nd-4.0",null],"heldby":["anu","asu","auburn","auckland","universityofcalifornia","brown","bu","columbia","cornell","dartmouth","duke","harvard","hawaii","iastate","illinois","iu","jhu","ksu","miami","monash","msu","northwestern","nypl","ox","princeton","smith","stanford","ua","uchicago","uiowa","umass","umich","umn","unc","unimelb","unsw","upenn","uq","usg","utexas","utoronto","uwo","virginia","wisc","yorku"],"collection_code":"ucsc","enum_pubdate":"1983","enum_pubdate_range":"1980-1989","dig_source":"google"}]' }
   end
-
   let(:rec) { CatalogRecord.new_from_doc(doc) }
+  let(:full_doc) { JSON.parse(File.open('spec/data/fullrecord.json').read) }
+  let(:full_rec) { CatalogRecord.new_from_doc(full_doc) }
 
   describe '#new_from_doc' do
     it 'extracts the fields we want' do
@@ -25,6 +26,11 @@ RSpec.describe CatalogRecord do
       expect(rec.authors).to eq(['Manderson, Lenore.', 'Australian National University.'])
       expect(rec.publisher).to eq(['Australian National University ;', 'Distributed by ANU Press,'])
       expect(rec.title_pub_date).to eq(['1983'])
+      expect(rec.subject).to eq(['Women', 'Women Singapore Economic conditions'])
+    end
+
+    it 'pulls the Rec Source Code from HOL$s' do
+      expect(full_rec.rec_src_code).to eq('UVA')
     end
 
     it 'pulls ht items from the ht_json' do
